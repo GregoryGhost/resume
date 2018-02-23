4a2d7525d5c5b8fed87881c8370d38fb02cbe5c1
Gregory <lobster287@gmail.com>
2018-02-23 17:46:07 +0700

    Added support html

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,17 @@ const PATHS = {
     build: path.join(__dirname, 'bin')
 };
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: path.join(PATHS.source, 'index.js'),
     output: {
         path: PATHS.build,
         filename: '[name].js'
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack app'
+        })
+    ]
 }
