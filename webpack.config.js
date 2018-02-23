8f45abd0c6055f4f21b625666442fc2d980b24cd
Gregory <lobster287@gmail.com>
2018-02-23 18:04:04 +0700

    Added support Pug

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,10 @@ const PATHS = {
 };
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const pug = require('pug');
+
+
+const compiledFunction = pug.compileFile(path.join(PATHS.source, 'index.pug'));
 
 module.exports = {
     entry: path.join(PATHS.source, 'index.js'),
@@ -13,9 +17,25 @@ module.exports = {
         path: PATHS.build,
         filename: '[name].js'
     },
+    module: {
+        rules: [{
+            test: /\.pug$/,
+            use: {
+                loader: 'pug-loader',
+                options: {
+                    pretty: true
+                }
+            }
+        }]
+    },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Webpack app'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            chunks: ['index'],
+            template: path.join(PATHS.source, './index.pug')
         })
     ]
 }
