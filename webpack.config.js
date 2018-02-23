96397bf9c12448fbd6afa8360cd2be45de997560
Gregory <lobster287@gmail.com>
2018-02-23 19:26:47 +0700

    Divided the config of webpack into modules for production and development, aso added config for the css, pug, js

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,59 @@
 const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const pug = require('./webpack/pug');
+const css = require('./webpack/css');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const PATHS = {
     source: path.join(__dirname, 'source'),
     build: path.join(__dirname, 'bin')
 };
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const pug = require('pug');
-
-
-const compiledFunction = pug.compileFile(path.join(PATHS.source, './pages/index/index.pug'));
-const compiledFunction2 = pug.compileFile(path.join(PATHS.source, './pages/blog/blog.pug'));
-
-module.exports = {
-    entry: {
-        'index': path.join(PATHS.source, 'pages/index/index.js'),
-        'blog': path.join(PATHS.source, 'pages/blog/blog.js')
+const common = merge([{
+        entry: {
+            'index': path.join(PATHS.source, 'pages/index/index.js'),
+            'blog': path.join(PATHS.source, 'pages/blog/blog.js')
+        },
+        output: {
+            path: PATHS.build,
+            filename: './js/[name].js'
+        },
+        output: {
+            path: PATHS.build,
+            filename: 'js/[name].js'
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                filename: 'index.html',
+                chunks: ['index', 'common'],
+                template: path.join(PATHS.source, './pages/index/index.pug')
+            }),
+            new HtmlWebpackPlugin({
+                filename: 'blog.html',
+                chunks: ['blog', 'common'],
+                template: path.join(PATHS.source, './pages/blog/blog.pug')
+            }),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: 'common'
+            })
+        ],
     },
-    output: {
-        path: PATHS.build,
-        filename: './js/[name].js'
-    },
-    module: {
-        rules: [{
-            test: /\.pug$/,
-            use: {
-                loader: 'pug-loader',
-                options: {
-                    pretty: true
-                }
-            }
-        }]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Webpack app'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            chunks: ['index'],
-            template: path.join(PATHS.source, './pages/index/index.pug')
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'blog.html',
-            chunks: ['blog'],
-            template: path.join(PATHS.source, './pages/blog/blog.pug')
-        })
-    ]
+    pug()
+]);
+
+module.exports = function(env) {
+    if (env === 'production') {
+        return merge([
+            common,
+            extractCSS(),
+            uglifyJS()
+        ])
+    }
+    if (env === 'develop') {
+        return merge([
+            common,
+            css()
+        ])
+    }
 }
