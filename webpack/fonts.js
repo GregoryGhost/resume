ff0b88677f4c7299033325750cd08b00d42ea1fd
Gregory <lobster287@gmail.com>
2018-07-08 04:15:21 +0700

    Fixing: webconfig for fonts - added svg font

--- webpack/fonts.js
+++ webpack/fonts.js
@@ -3,13 +3,13 @@ module.exports = function() {
     module: {
       rules: [
         {
-          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
           loader: 'file-loader',
           options: {
-            name:'fonts/[name].[ext]'
-          },
-        },
-      ],
-    },
-  };
+            name: 'fonts/[name].[ext]'
+          }
+        }
+      ]
+    }
+  }
 };
